31fac4c3ac400cc9fef02861d647ffe6c2b3c549
evandomingos <edomingos2019@gmail.com>
2023-05-01 18:51:16 -0400

    Create test_build.py

---
+++ test_build.py
@@ -0,0 +1,10 @@
+# Various Libraries, some possibly redundant
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import scale
+import torch
+
+
+
+dataset = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/00265/CASP.csv", header=0)
+
