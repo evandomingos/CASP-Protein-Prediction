69c4ed3a46390d36aefd11da3e157c1789bcd30d
evandomingos <edomingos2019@gmail.com>
2023-05-02 09:47:44 -0400

    Update test_build.py

--- test_build.py
+++ test_build.py
@@ -2,9 +2,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import scale
-import torch
-
 
 
 dataset = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/00265/CASP.csv", header=0)
+
+protein = dataset.dropna()
 
