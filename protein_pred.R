21388a5146401a70bb208c576bbb09b951079763
evandomingos <edomingos2019@gmail.com>
2023-05-07 17:11:17 -0400

    Update protein_pred.R

--- protein_pred.R
+++ protein_pred.R
@@ -2,20 +2,148 @@
 library(tidyverse)  
 library(caret)
 library(tensorflow) 
-library(ranger)     #Random forest (more efficient than rf library for higher dimensional data)
-library(nnet)  #Neural net
-library(keras) 
+library(ranger) # Random forest with Ranger (more efficient than rf library for higher dimensional data)
+library(randomForest) # Random Forest
+library(xgboost) # For Gradient Boosted Tree
+library(nnet)  # Neural net
+library(keras)
 library(gtable)
-library(luz) # High-level interface for torch
-library(torchvision) # For datasets and image transformation
-library(torchdatasets) # For datasets we are going to use
-library(randomForest)
 library(zeallot)
 library(rpart)
 library(rpart.plot)
 library(torch)
-library(torchvision)
-library(xgboost)
+library(luz) # High-level interface for torch
+library(torchvision) # For datasets and image transformation
+library(GMDHreg) # Library for Group Method of Data Handling (GMDH) Nueral Network
+
+# Load CASP 5.9 Set from 
+dataset <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/00265/CASP.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# Remove missing data (none in CASP dataset), create protein set
+protein <- na.omit(dataset)
+
+#n number of protein residues: 45730 obs
+n <- nrow(protein)
+# For reproduction
+set.seed(7)
+
+# Number of test samples = 1/3 observations
+ntest <- trunc(n / 3)
+#randomly select test IDs from 
+testid <- sample(1:n, ntest)
+
+# Set PyTorch seed
+torch_manual_seed(7)
+
+# In this segment,  only Gradient Boosted tree uses
+# Scale variables, separate x labels ,y target var. Remove intercepts
+x <- scale(model.matrix(protein$RMSD ~ . - 1, data = protein))
+length(x)
+y <- protein$RMSD
+
+
+
+
+
+
+
+# First Method
+# Random Forest regression tree with the obejctive of plotting feature important and purposely overfitting a model
+# Very easy to produce good looking tree, extremely overfitted and validation MAE explodes
+
+library(randomForest)
+
+
+# Split the data into training and testing sets
+
+# *** Un-comment these blocks to run the Random Forest Model. Commented out so the train_id object does not affect Nueral Networks ***
+
+# For reproducibility
+set.seed(13)
+#train_id <- sample(1:n, floor(n * 0.6), replace = FALSE)
+#valid_id <- sample(setdiff(1:n, train_id), floor(n * 0.2), replace = FALSE)
+#test_id <- setdiff(1:n, union(train_id, valid_id))
+
+# Create extra holdout set, "valid"
+#train_x <- x[train_id, ]
+#train_y <- y[train_id]
+#valid_x <- x[valid_id, ]
+#valid_y <- y[valid_id]
+#test_x <- x[test_id, ]
+#test_y <- y[test_id]
+
+
+# Train the random forest regression model
+rf_model <- randomForest(train_x, train_y, ntree = 500)
+
+# Predict RMSD for test set using the trained model
+pred_valid <- predict(rf_model, valid_x)
+pred_test <- predict(rf_model, test_x)
+
+# Calculate mean squared error on the test set and validation (extra holdout data)
+mae_valid <- mean(abs(pred_valid - test_y))
+mae_test <- mean(abs(pred_test-test_y))
+
+# MAE 2.5555 on test set 
+# After purposely overfitting as much as possible (try nrounds >>500) this MAE of 2.55 appears to be the absolute minimume error achievable
+mae_test
+
+# MAE 6.161 on validation, seocnd holdout set
+mae_valid
+
+# Conventional Regression Tree for the purpose of displaying featurew importance
+protein_tree1 <- rpart(RMSD ~ ., data = protein[train_id,], control = rpart.control(minsplit = 1))
+
+# Predict on Training set
+train_pred_protein_tree1 <- predict(protein_tree1, protein[train_id,])
+# Calculate MAE on training set
+train_tree1_mae <- mean(abs(train_pred_protein_tree1-protein$RMSD[train_id]))
+# Train MAE 5.49
+train_tree1_mae
+
+# Predict on Test Set, Calculate MAE
+test_pred_protein_tree1 <- predict(protein_tree1, newdata = protein[test_id, ])
+
+# Calculate MAE for test set
+mae_protein_tree1 <- mean(abs(pred_protein_tree1 - y[testid]))
+# Test MAE 5.49
+mae_protein_tree1
+
+rpart.plot(protein_tree1)
+
+  
+
+#Gradient Boosted Tree
+# Gradient Boosted Regression Tree with MAE Objective Function
+gdboosteds_protein <- xgboost(data = x[-testid,], label = y[-testid], nrounds = 10000, objective = "reg:absoluteerror")
+gdboost_test_pred <- predict(gdboosteds_protein, x[testid, ])
+# For set.seed(13), nrounds = 10,000, minsplit = DEFAULT, trained MAE = 2.628
+
+# Calculate MSE
+mse <- mean((gdboost_test_pred-y[testid])^2)
+# 19.3254 MSE for set.seed(13), nround = 10,000
+mse
+
+
+# Calculate MAE on test set
+mae <- mean(abs(gdboost_test_pred-y[testid]))
+# 2.979239 MAE for set.seed(7), nround = 10000
+mae
+
+
+importance_matrix <- xgb.importance(model = gdboosteds_protein)
+importance_matrix
+xgb.plot.importance(importance_matrix, col = rgb(124/256,148/256,198/256), xlab = "Importance (0-1)", 
+                    ylab = "Feature")
+
+
+
+
+
+
+
+
+
 
 # Load CASP 5.9 Set from 
 dataset <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/00265/CASP.csv", header = TRUE, stringsAsFactors = FALSE)
@@ -41,86 +169,7 @@ x <- scale(model.matrix(protein$RMSD ~ . - 1, data = protein))
 y <- protein$RMSD
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# First Method
-# Random Forest regression tree with the obejctive of plotting feature important and purposely overfitting a model
-# Very easy to produce good looking tree, extremely overfitted and validation MAE explodes
-
-library(randomForest)
-
-
-# Split the data into training and testing sets
-
-# *** Un-comment these blocks to run the Random Forest Model. Commented out so the train_id object does not affect Nueral Networks ***
-
-# For reproducibility
-set.seed(13)
-#train_id <- sample(1:n, floor(n * 0.6), replace = FALSE)
-#valid_id <- sample(setdiff(1:n, train_id), floor(n * 0.2), replace = FALSE)
-#test_id <- setdiff(1:n, union(train_id, valid_id))
-
-
-#train_x <- x[train_id, ]
-#train_y <- y[train_id]
-#valid_x <- x[valid_id, ]
-#valid_y <- y[valid_id]
-#test_x <- x[test_id, ]
-#test_y <- y[test_id]
-
-
-# Train the random forest regression model
-rf_model <- randomForest(train_x, train_y, ntree = 500)
-
-# Predict RMSD for test set using the trained model
-pred_valid <- predict(rf_model, valid_x)
-pred_test <- predict(rf_model, test_x)
-
-# Calculate mean squared error on the test set
-mae_valid <- mean(abs(pred_valid - test_y))
-mae_test <- mean(abs(pred_test-test_y))
-
-#MAE 2.5555 on test set with  
-# After purposely overfitting as much as possible (try nrounds >>500) this MAE of 2.55 appears to be the absolute minimume error achievable
-mae_test
-
-#6.161
-mae_valid
-
-# Strict Regression Tree
-protein_tree1 <- rpart(RMSD ~ ., data = protein[train_id,], control = rpart.control(minsplit = 1))
-
-# Predict on Training set, goal was to minimize MAE
-train_pred_protein_tree1 <- predict(protein_tree1, protein[train_id,])
-# Calculate MAE on training set
-train_tree1_mae <- mean(abs(train_pred_protein_tree1-protein$RMSD[train_id]))
-train_tree1_mae
-
-# Predict on Test Set, Calculate MAE
-test_pred_protein_tree1 <- predict(protein_tree1, newdata = protein[test_id, ])
-
-# Calculate MAE for test set
-mae_protein_tree1 <- mean(abs(pred_protein_tree1 - y[testid]))
-mae_protein_tree1
-
-rpart.plot(protein_tree1)
-
-  
+# PYTORCH NEURAL NETWORKS
 
 #create NN with four layers, input, 50 node hidden layer, Relu activation, dropout layer 0.4 
 modnn <- nn_module(
@@ -178,8 +227,8 @@ abline(a = 0, b = 1, col = "purple")
 
 
 
-#best model
-#2nd Model, Add elu hidden layer
+# Best model by MAE & complexity 
+# 2nd Model evalauted, Addition of ELU hidden layer
 modnn2 <- nn_module(
   initialize = function(input_size) {
     self$hidden1 <- nn_linear(input_size, 50)
@@ -211,7 +260,7 @@ modnn2 <- set_hparams( setup(modnn2 ,
                       input_size = ncol(x))
 
 
-#training
+# Training for ModNN2 
 #210sec per epoch
 fitted2 <-   fit(modnn2,
                 data = list(x[-testid, ],
@@ -527,8 +576,8 @@ abline(a = 0, b = 1, col = "purple")
 
 
 
-# GMDH NUeral Network
-# USe of diufferent 
+# Separation of features Nueral Network
+# Attempt to isloate Polar surface areas which appear to be important for RMSD prediction, likely a factor of Van Der Whaalen 
 modnn6 <- nn_module(
   initialize = function(input_size) {
     self$hidden1 <- nn_linear(input_size, 50)
@@ -573,16 +622,16 @@ modnn6 <- nn_module(
   }
 )
 
-modnn4_setup <- setup(modnn6 ,
+modnn6_setup <- setup(modnn6 ,
                       loss = nn_mse_loss(),
                       optimizer = optim_rmsprop,
                       metrics = list(luz_metric_mae()))
 
-modnn6 <- set_hparams(modnn4_setup, input_size = ncol(x))
+modnn6 <- set_hparams(modnn6_setup, input_size = ncol(x))
 
-# Training ModNN6 GMDH Nueral Network
+# Training ModNN6 Separation of Features Nueral Network
 
-#210sec per epoch
+# Approximately 240 seconds for each epoch
 fitted6 <- fit(modnn6,
                data = list(x[-testid, ],
                            matrix(y[-testid], ncol = 1)),
@@ -594,28 +643,78 @@ fitted6 <- fit(modnn6,
 
 
 
-#Gradient Boosted Tree
-# Gradient Boosted Regression Tree with MAE Objective Function
-gdboosteds_protein <- xgboost(data = x[-testid,], label = y[-testid], nrounds = 10000, objective = "reg:absoluteerror")
-gdboost_test_pred <- predict(gdboosteds_protein, x[testid, ])
 
-# Calculate MSE
-mse <- mean((gdboost_test_pred-y[testid])^2)
-#19.3254 MSE for set.seed(7), nround = 10000
-mse
+# Group Method of Data Handling Neural Network (GMDH)
+# Define the base models for the GMDH NN
+
+# GMDH function is from GMDHreg library
+# Training Multilayered Iterative (MIA) GMDH Neural Network
+
+# Fit GMDH NN model
+modnn7 <- gmdh.mia(X = as.matrix(protein[-testid, 2:10]), 
+                      y = as.matrix(protein[-testid, 1]), 
+                      prune = ncol(protein[-testid, 2:10]), 
+                      criteria = "PRESS")
 
 
-# Calculate MAE
-mae <- mean(abs(gdboost_test_pred-y[testid]))
-#2.979239 MAE for set.seed(7), nround = 10000
+# Predict on validation set for GMDH NN
+pred.modnn7 <- predict(modnn7, as.matrix(protein[testid, 2:10]))
+
+sum(is.na(pred.modnn7[,1]))
+# For set.seed(13), there are 27 NA values
+
+# Get row numbers for NA vlaues
+missing_rows <- which(is.na(pred.modnn7[,1]))
+missing_rows
+
+pred.modnn7_clean <- pred.modnn7[-missing_rows, ]
+# Should be 0
+sum(is.na(pred.modnn7_clean))
+
+# Remove missing rows from the test set
+protein.test <- protein[testid,][-missing_rows,]
+# Calculate MAE on test set
+mae <- mean(abs(pred.modnn7_clean - protein.test[,1]))
+# With set.seed(13), MAE is 4.359865
 mae
 
+# Plot predicted RMSD against actual RMSD
+plot(protein.test[,1], pred.modnn7_clean, xlab = "Actual RMSD (Angstrom)", ylab = "Predicted RMSD (Angstrom)", main = "NN8: 141 Layer MIA GMDH Neural Network")
+abline(a = 0, b = 1, col = "purple")
 
-importance_matrix <- xgb.importance(model = gdboosteds_protein)
-importance_matrix
-xgb.plot.importance(importance_matrix, col = rgb(124/256,148/256,198/256), xlab = "Importance (0-1)", 
-                    ylab = "Feature")
 
+
+
+
+
+
+# Fit GMDH GIA NN model
+modnn8 <- gmdh.gia(X = as.matrix(protein[-testid, 2:10]), 
+                   y = as.matrix(protein[-testid, 1]), 
+                   prune = 9*2, 
+                   criteria = "testr")
+
+ncol(protein[-testid, 2:10])
+# Predict on validation set for GMDH NN
+pred.modnn8 <- predict(modnn8, as.matrix(protein[testid, 2:10]))
+
+sum(is.na(pred.modnn8[,1]))
+# For set.seed(13), there are 27 NA values
+
+# Get row numbers for NA vlaues
+missing_rows <- which(is.na(pred.modnn7[,1]))
+missing_rows
+
+pred.modnn7_clean <- pred.modnn7[-missing_rows, ]
+# Should be 0
+sum(is.na(pred.modnn7_clean))
+
+# Remove missing rows from the test set
+protein.test <- protein[testid,][-missing_rows,]
+# Calculate MAE on test set
+mae <- mean(abs(pred.modnn7_clean - protein.test[,1]))
+# With set.seed(13), MAE is 4.36
+mae
 
 
 
